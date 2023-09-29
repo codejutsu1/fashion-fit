740337422f0461aaef228aae4f7e72906d9537cf
codejutsu1 <codejutsu@protonmail.com>
2023-09-29 20:28:17 +0100

    worked on the about us hero page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="overflow-hidden">
     <section class="bg-about bg-cover py-14">
-      <div>
-        <div>
-          <h1>
+      <div class="w-1/2 mx-auto py-14 flex items-center justify-center px-1">
+        <div class="text-center">
+          <h1 class="text-5xl font-semibold leading-[64.8px] text-[#FFFFFF]">
             How did FASHION FIT become a huge success
           </h1>
-          <p>Get inspired as you read about us</p>
+          <p class="text-[#FFFFFF] text-2xl py-2">Get inspired as you read about us</p>
           <button class="bg-[#307098] py-3 px-9 mt-10 font-bold leading-5 text-[#FFFFFF]">
             Shop Now
         </button>
