06c081ffb8a1b3d4cdcb70d785b9ecd58b84c35b
codejutsu1 <codejutsu@protonmail.com>
2023-09-29 21:01:49 +0100

    worked on the read our story section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,6 +13,27 @@
         </div>
       </div>
     </section>
+
+    <section class="py-14">
+      <div class="w-5/6 mx-auto">
+        <div class="relative py-10">
+          <div class="ml-32">
+            <img class="z-40 relative" src="/images/pictures/picture-3.jpg" alt="Cute Couples">
+          </div>
+          <div class="flex justify-end bg-[#F1E0D2] absolute top-[15%] py-32">
+            <div class="w-1/2">
+              <div class="w-[65%] mx-auto text-[#141415]">
+                <h1 class="text-4xl font-semibold">Read our Story</h1>
+                <span class="py-3 block text-2xl">How did we start?</span>
+                <p class="font-normal">
+                  Our fashion brand is an online brand that was established in 2010 to enable women access premium wears at an affordable price. Over time, we have evolved and included men as a part of our target audience. We are dedicated to serving fashion needs as best as we can while also ensuring that they are of the best quality.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
