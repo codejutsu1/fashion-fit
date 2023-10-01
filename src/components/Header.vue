f1bac37a6e4f406ca629fc65d52b4b0da20d37f8
codejutsu1 <codejutsu@protonmail.com>
2023-10-01 03:43:40 +0100

    made the navigation reponsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,26 +9,29 @@
                 <img src="/images/logo/logo.svg" alt="Fashion Fit Logo">
             </div>
             <nav>
-            <ul class="flex space-x-16 font-bold text-base text">
-                <li>
-                    <RouterLink class="text-[#307098]" to="/">Home</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/about">About us</RouterLink>
-                </li>
-                <li>
-                    <a href="#">Collections</a>
-                </li>
-                <li>
-                    <RouterLink to="/contact-us">Contact us</RouterLink>
-                </li>
-            </ul>
+                <ul class="hidden lg:flex space-x-16 font-bold text-base text">
+                    <li>
+                        <RouterLink class="text-[#307098]" to="/">Home</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/about">About us</RouterLink>
+                    </li>
+                    <li>
+                        <a href="#">Collections</a>
+                    </li>
+                    <li>
+                        <RouterLink to="/contact-us">Contact us</RouterLink>
+                    </li>
+                </ul>
             </nav>
-            <div class="flex space-x-5">
-            <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/search.svg" alt="Search Icon">
-            <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/person.svg" alt="Person Icon">
-            <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/cart.svg" alt="Cart Icon">
-            <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/wishlist.svg" alt="Wishlist Icon">
+            <div class="hidden lg:flex space-x-5">
+                <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/search.svg" alt="Search Icon">
+                <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/person.svg" alt="Person Icon">
+                <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/cart.svg" alt="Cart Icon">
+                <img class="w-full h-full object-fit cursor-pointer" src="/images/icons/wishlist.svg" alt="Wishlist Icon">
+            </div>
+            <div class="lg:hidden">
+                <img src="/images/icons/menu.svg" alt="Menu Bar">
             </div>
         </div>
     </header>
