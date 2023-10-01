3c7e4e8faf02604e7a5550e19f8b2b83a118fe5c
codejutsu1 <codejutsu@protonmail.com>
2023-10-01 05:23:08 +0100

    working on the contact us page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -17,17 +17,17 @@ import Footer from '../components/Footer.vue'
         </section>
 
         <section class="py-14">
-            <div class="w-3/5 mx-auto text-[#141415]">
-                <h1 class="text-2xl font-medium text-center">Do you have any enquires?</h1>
-                <p class="text-center font-light py-3">We are available 24 hours, 7 days.</p>
+            <div class="lg:w-3/5 w-4/5 mx-auto text-[#141415]">
+                <h1 class="lg:text-2xl text-xl font-medium text-center">Do you have any enquires?</h1>
+                <p class="text-center text-sm lg:text-base font-light py-3">We are available 24 hours, 7 days.</p>
 
-                <div class="flex pt-10 justify-between">
-                    <div class="flex justify-between gap-x-5">
+                <div class="lg:flex pt-10 justify-between">
+                    <div class="flex justify-between lg:gap-x-5">
                         <div class="w-[100px] h-[100px] p-[30px] rounded-[60px] bg-[#F7EDE5] contact flex items-center justify-center">
                             <img src="/images/icons/phone.svg" alt="Phone">
                         </div>
                         <div>
-                            <h1 class="text-xl font-medium">Give us a call today!</h1>
+                            <h1 class="lg:text-xl text-base font-medium">Give us a call today!</h1>
 
                             <div class="pt-4 text-[#2BA646] space-y-1 font-medium">
                                 <span class="block">
