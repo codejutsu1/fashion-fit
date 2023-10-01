ec246f0b51ebe14494cf4454d5e10d520f0581db
codejutsu1 <codejutsu@protonmail.com>
2023-10-01 04:47:29 +0100

    made the home page responsive

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,9 +1,9 @@
 <template>
-    <section class="py-14">
+    <section class="py-10 lg:py-14">
       <div class="mx-auto w-5/6 text-[#141415]">
-        <h1 class="text-4xl font-semibold leading-[50px] text-center">Reviews</h1>
-        <p class="text-center text-2xl">What our clients say about us</p>
-        <div class="grid grid-cols-3 py-10 mx-auto">
+        <h1 class="text-2xl lg:text-4xl font-semibold leading-[50px] text-center">Reviews</h1>
+        <p class="text-center text-base lg:text-2xl">What our clients say about us</p>
+        <div class="md:grid grid-cols-3 py-10 mx-auto">
           <div>
             <div class="flex justify-center">
               <div class="relative inline-block">
@@ -13,7 +13,7 @@
                 </div>
               </div>
             </div>
-            <div class="py-7 text-center px-10">
+            <div class="py-7 text-center lg:px-10">
               <span class="font-bold text-[#141415]">Halimat</span>
               <p class="text-sm text-[#141415] py-2">
                 <span class="text-[#307098] font-bold">Fashion Fit</span> is the best platform to get your outfits, So affordable & of good quality. I have been patronizing them for years.
@@ -30,7 +30,7 @@
                 </div>
               </div>
             </div>
-            <div class="py-7 text-center px-10">
+            <div class="py-7 text-center lg:px-10">
               <span class="font-bold text-[#141415]">Michael</span>
               <p class="text-sm text-[#141415] py-2">
                 <span class="text-[#307098] font-bold">Fashion Fit</span> always deliver on time and their wears are top notch. I will recommend them anyday anytime.
@@ -47,7 +47,7 @@
                 </div>
               </div>
             </div>
-            <div class="py-7 text-center px-10">
+            <div class="py-7 text-center lg:px-10">
               <span class="font-bold text-[#141415]">Susan</span>
               <p class="text-sm text-[#141415] py-2">
                 The wears I get on <span class="text-[#307098] font-bold">Fashion Fit</span>   are so affordable. I get my orders on time & they are always great. A must have.
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,11 @@ import Reviews from '../components/Reviews.vue'
 </script>
 <template>
   <main class="overflow-hidden">
-    <section class="pt-12 pb-10 w-5/6 mx-auto">
+    <section class="pt-12 pb-10 w-full lg:w-5/6 mx-auto">
       <div class="text-center text-[#141415]">
-        <h1 class="text-6xl font-bold leading-[89.6px]">Find your <span class="text-[#FFCC4D]">style</span> in our collection</h1>
-        <div class="w-3/5 px-4 mx-auto">
-          <p class="text-2xl leading-9">Fashion is a way of life. Experience fashion at its best as you shop from our collections </p>
+        <h1 class="text-4xl lg:text-6xl font-bold lg:leading-[89.6px]">Find your <span class="text-[#FFCC4D]">style</span> in our collection</h1>
+        <div class="text-center w-4/5 lg:w-3/5 px-4 mx-auto pt-2 lg:pt-0">
+          <p class="text-base lg:text-2xl lg:leading-9">Fashion is a way of life. Experience fashion at its best as you shop from our collections </p>
         </div>
         <button class="bg-[#307098] py-3 px-9 mt-10 font-bold leading-5 text-[#FFFFFF]">
           Shop Now
@@ -17,7 +17,7 @@ import Reviews from '../components/Reviews.vue'
       <div class="pt-10">
         <div class="relative">
           <img class="w-full h-full object-fit" src="/images/pictures/picture-1.jpg" alt="Landing page Image">
-          <div class="absolute flex gap-2 items-center py-4 px-5 rounded-[40px] landing bg-[#FFFFFF] right-0 bottom-40">
+          <div class="absolute hidden lg:flex gap-2 items-center py-4 px-5 rounded-[40px] landing bg-[#FFFFFF] right-0 bottom-40">
             <img src="/images/socials/whatsapp.svg" alt="Whatsapp">
             <span class="text-xl font-medium text-[#0A3B5A]">Chat with an expert</span>
           </div>
@@ -27,10 +27,10 @@ import Reviews from '../components/Reviews.vue'
 
     <section class="bg-[#F9F3EE] py-12">
       <div class="w-11/12 mx-auto text-[#141415]">
-        <h1 class="font-semibold text-4xl leading-[50px]">New Collection!</h1>
-        <p class="text-2xl">Our collections are elegant and of the best quality</p>
+        <h1 class="font-semibold text-2xl lg:text-4xl lg:leading-[50px]">New Collection!</h1>
+        <p class="text-base lg:text-2xl">Our collections are elegant and of the best quality</p>
 
-        <div class="grid grid-cols-3 gap-8 pt-10">
+        <div class="lg:grid grid-cols-3 gap-8 pt-10">
           <div class="w-full overflow-hidden">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-1.jpg" alt="Jean Jacket">
@@ -44,7 +44,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div> 
           
-          <div class="w-full overflow-hidden">
+          <div class="w-full overflow-hidden hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-2.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -57,7 +57,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div> 
 
-          <div class="w-full overflow-hidden">
+          <div class="w-full overflow-hidden hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-3.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -70,7 +70,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div> 
 
-          <div class="w-full overflow-hidden">
+          <div class="w-full overflow-hidden hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-4.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -83,7 +83,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div> 
 
-          <div class="w-full overflow-hidden">
+          <div class="w-full overflow-hidden hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-5.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -96,7 +96,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div> 
 
-          <div class="w-full overflow-hidden">
+          <div class="w-full overflow-hidden hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/collection/collection-6.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -119,15 +119,15 @@ import Reviews from '../components/Reviews.vue'
     </section>
 
     <section class="py-14">
-      <div class="w-5/6 mx-auto flex justify-between">
+      <div class="w-5/6 mx-auto lg:flex justify-between">
         <div>
           <img class="w-full h-full object-fit" src="/images/pictures/picture-2.jpg" alt="A young woman smiling holding a bottle.">
         </div>
-        <div class="flex items-center justify-center">
-          <div class="text-center px-10">
-            <h1 class="text-4xl font-semibold leading-[50px] text-outfit">Outfit for every occassion</h1>
-            <p class="text-2xl">Explore outfits that are perfect for your next beach outing</p>
-            <div class="flex justify-center py-5">
+        <div class="flex items-center justify-center pt-4 lg:pt-0">
+          <div class="text-center px-2 lg:px-10">
+            <h1 class="text-2xl lg:text-4xl font-semibold lg:leading-[50px] text-outfit">Outfit for every occassion</h1>
+            <p class="text-base pt-2 lg:text-2xl">Explore outfits that are perfect for your next beach outing</p>
+            <div class="flex justify-center pt-5">
               <button class="bg-[#307098] py-3 px-9 mx-auto font-bold leading-5 text-[#FFFFFF]">
                 Shop now
               </button>
@@ -139,10 +139,10 @@ import Reviews from '../components/Reviews.vue'
 
     <section class="best-sellers py-14">
       <div class="w-11/12 mx-auto relative">
-        <div class="best-sellers-div inline-block p-5 text-2xl font-medium">
+        <div class="best-sellers-div inline-block p-5 text-xl lg:text-2xl font-medium">
           Our Best Sellers
         </div>
-        <div class="grid grid-cols-3 gap-8">
+        <div class="md:grid grid-cols-3 gap-8">
           <div class="w-full overflow-hidden best-seller-grid">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/best-sellers/best-seller-1.jpg" alt="Jean Jacket">
@@ -164,7 +164,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div>
 
-          <div class="w-full overflow-hidden best-seller-grid">
+          <div class="w-full overflow-hidden best-seller-grid hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/best-sellers/best-seller-2.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -185,7 +185,7 @@ import Reviews from '../components/Reviews.vue'
             </div>
           </div>
 
-          <div class="w-full overflow-hidden best-seller-grid">
+          <div class="w-full overflow-hidden best-seller-grid hidden lg:block">
             <div class="relative">
               <img class="w-full h-full object-fit" src="/images/best-sellers/best-seller-3.jpg" alt="Jean Jacket">
               <div class="absolute w-11 h-11 rounded-full wish-icon bottom-5 right-7 flex items-center justify-center">
@@ -219,17 +219,17 @@ import Reviews from '../components/Reviews.vue'
 
     <section class="bg-[#FDFBF9] py-14">
       <div class="w-5/6 mx-auto">
-        <h1 class="text-4xl font-semibold leading-[50px] text-center">Benefits</h1>
-        <p class="text-center text-2xl">Why patronize us?</p>
-        <div class="grid grid-cols-4 justify-between py-10 mx-auto px-10 text-[#141415]">
+        <h1 class="text-2xl lg:text-4xl font-semibold lg:leading-[50px] text-center">Benefits</h1>
+        <p class="text-center text-base lg:text-2xl">Why patronize us?</p>
+        <div class="grid grid-cols-2 lg:grid-cols-4 justify-between py-10 mx-auto lg:px-10 text-[#141415]">
           <div>
             <div class="flex justify-center">
               <div class="w-32 h-32 rounded-full bg-[#EEF2F5] flex justify-center items-center">
                 <img src="/images/icons/delivery.svg" alt="Prompt Delivery">
               </div>
             </div> 
-            <div class="px-10">
-              <h1 class="text-center py-2 font-bold">Prompt Delivery</h1>
+            <div class="lg:px-8">
+              <h1 class="text-center py-2 font-bold text-sm lg:text-base">Prompt Delivery</h1>
               <p class="font-medium text-xs text-center">You get your orders without delay</p>
             </div>
           </div>
@@ -240,8 +240,8 @@ import Reviews from '../components/Reviews.vue'
                 <img src="/images/icons/quality.svg" alt="Prompt Delivery">
               </div>
             </div> 
-            <div class="px-10">
-              <h1 class="text-center py-2 font-bold">Quality Products</h1>
+            <div class="lg:px-8">
+              <h1 class="text-center py-2 font-bold text-sm lg:text-base">Quality Products</h1>
               <p class="font-medium text-xs text-center">Our products are super affordable and of good quality</p>
             </div>
           </div>
@@ -252,8 +252,8 @@ import Reviews from '../components/Reviews.vue'
                 <img src="/images/icons/support.svg" alt="Prompt Delivery">
               </div>
             </div> 
-            <div class="px-10">
-              <h1 class="text-center py-2 font-bold">Customer support 24/7</h1>
+            <div class="lg:px-8">
+              <h1 class="text-center py-2 font-bold text-sm lg:text-base">Customer support 24/7</h1>
               <p class="font-medium text-xs text-center">We are ready to help you with any challenge.</p>
             </div>
           </div>
@@ -264,8 +264,8 @@ import Reviews from '../components/Reviews.vue'
                 <img src="/images/icons/discount.svg" alt="Prompt Delivery">
               </div>
             </div> 
-            <div class="px-10">
-              <h1 class="text-center py-2 font-bold">10% off first order</h1>
+            <div class="lg:px-8">
+              <h1 class="text-center py-2 font-bold text-sm lg:text-base">10% off first order</h1>
               <p class="font-medium text-xs text-center">Special discount for all new clients</p>
             </div>
           </div>
@@ -276,19 +276,19 @@ import Reviews from '../components/Reviews.vue'
 
     <section class="bg-[#F9F3EE] py-14">
       <div>
-        <h1 class="text-4xl font-semibold leading-[50px] text-center">Our Blog</h1>
-        <p class="text-center text-2xl">Stay to date about fashion</p>
+        <h1 class="text-2xl lg:text-4xl font-semibold lg:leading-[50px] text-center">Our Blog</h1>
+        <p class="text-center text-base lg:text-2xl">Stay to date about fashion</p>
 
-        <div class="w-11/12 grid grid-cols-3 mx-auto pt-10 gap-8">
+        <div class="w-11/12 md:grid grid-cols-3 mx-auto pt-10 gap-8">
           <div>
             <div class="border border-[#F1E0D2]">
               <img class="object-fit w-full h-full" src="/images/blog/blog-1.jpg" alt="A woman holding some set of clothes.">
             </div>
             <div class="py-4 text-[#141415]">
-              <span class="font-medium text-xl">
+              <span class="font-medium text-base lg:text-xl">
                 The ultimate fashion guide is here...
               </span>
-              <p class="py-2">
+              <p class="py-2 text-xs lg:text-base">
                 The ultimate fashion guide will help you to learn how to put your outfits together and look great without breaking the bank. Save more and shop ... <span class="underline font-semibold text-[#307098]">Read More</span>
               </p>
             </div>
@@ -299,10 +299,10 @@ import Reviews from '../components/Reviews.vue'
               <img class="object-fit w-full h-full" src="/images/blog/blog-2.jpg" alt="A beautiful woman in pink.">
             </div>
             <div class="py-4 text-[#141415]">
-              <span class="font-medium text-xl">
+              <span class="font-medium text-base lg:text-xl">
                 The Beauty of Monochromic outfits...
               </span>
-              <p class="py-2">
+              <p class="py-2 text-xs lg:text-base">
                 Monochromic fashion can never go wrong, learn how to style your outfits to produce great looks. The first thing you need to know about this is you... <span class="underline font-semibold text-[#307098]">Read More</span>
               </p>
             </div>
@@ -313,10 +313,10 @@ import Reviews from '../components/Reviews.vue'
               <img class="object-fit w-full h-full" src="/images/blog/blog-3.jpg" alt="A well designed picture with leaves.">
             </div>
             <div class="py-4 text-[#141415]">
-              <span class="font-medium text-xl">
+              <span class="font-medium text-base lg:text-xl">
                 Pick the right accessories for your outfits...
               </span>
-              <p class="py-2">
+              <p class="py-2 text-xs lg:text-base">
                 Pairing your outfits with the right accessories is a great way to upscale your fashion sense and look outstanding. Accessories are very ... <span class="underline font-semibold text-[#307098]">Read More</span>
               </p>
             </div>
