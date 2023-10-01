13e0919c29e7ef2a9909d88dcf22fbcda211232a
codejutsu1 <codejutsu@protonmail.com>
2023-10-01 03:03:25 +0100

    worked on the contact us landing page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -7,11 +7,11 @@ import Footer from '../components/Footer.vue'
 
 <template>
     <main>
-        <section class="bg-contact h-screen bg-center bg-no-repeat">
-            <div>
-                <div>
-                    <span class="block">Need help?</span>
-                    <p>Get in touch</p>
+        <section class="bg-contact h-screen bg-cover bg-center bg-no-repeat flex items-end">
+            <div class="w-1/2 mx-auto flex justify-center h-2/5">
+                <div class="text-[#141415]">
+                    <span class="block text-2xl text-center ">Need help?</span>
+                    <p class="text-5xl leading-[67px] text-center font-semibold">Get in touch</p>
                 </div>
             </div>
         </section>
