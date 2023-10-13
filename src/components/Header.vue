332648a5dd0f518472ff53b0c7a2816348d5a2a0
codejutsu1 <codejutsu@protonmail.com>
2023-10-13 18:00:08 +0100

    added animation for the responsive menubar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -100,52 +100,54 @@ function closeMobile() {
             </div>
             
             <!-- Responsive mobile -->
-            <div class="fixed lg:hidden w-[70%] h-3/5 border-2 bg-[#FFFFFF] top-16 right-0 z-40 pb-8 transition ease-in duration-500" v-if="isOpen">
-                <!-- <div class="fixed bg-[#D9D9D9] inset-x-0 inset-y-0 overflow-hidden opacity-40"></div> -->
-                <div class="flex justify-between pt-8 px-4">
-                    <div class="border flex space-x-3 w-4/5 px-4 res-search-input">
-                        <img class="w-4" src="/images/icons/search-black.svg" alt="Search Icon">
-                        <div class="-full h-full">
-                            <input type="text" class="h-full w-full focus:outline-none placeholder:text-xs placeholder:font-medium placeholder:text-[#141415]" placeholder="Search" name="search">
+            <Transition>
+                <div class="fixed lg:hidden w-[70%]  border bg-[#FFFFFF] top-16 right-0 z-40" v-if="isOpen">
+                    <!-- <div class="fixed bg-[#D9D9D9] inset-x-0 inset-y-0 overflow-hidden opacity-40"></div> -->
+                    <div class="flex justify-between pt-4 pl-4 pr-2">
+                        <div class="border flex space-x-3 w-4/5 px-4 res-search-input">
+                            <img class="w-4" src="/images/icons/search-black.svg" alt="Search Icon">
+                            <div class="w-full h-full cursor-pointer">
+                                <input type="text" class="h-full w-full focus:outline-none placeholder:text-xs placeholder:font-medium placeholder:text-[#141415]" placeholder="Search" name="search">
+                            </div>
+                        </div>
+                        <div @click="isOpen = close" class="flex items-center pl-4 pr-2 py-4">
+                            <img class="w-3 h-full object-fit" src="/images/icons/close.svg" alt="Close SVG">
                         </div>
                     </div>
-                    <div @click="isOpen = !isOpen" class="flex items-center p-4">
-                        <img class="w-4" src="/images/icons/close.svg" alt="Close SVG">
-                    </div>
-                </div>
-                <nav>
-                    <ul class="pt-4 pb-8 text-[#141415]">
-                        <li>
-                            <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/">Home</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/about">About us</RouterLink>
-                        </li>
-                        <li @click="mobileCollectionOpen = true" class="flex justify-between items-center pr-8 border-b border-[#CAD7DF]">
-                            <a class=" px-4 py-3 w-full block" href="#">Collections</a>
-                            <img src="/images/icons/right.svg" alt="Right">
-                        </li>
-                        <li>
-                            <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/contact-us">Contact us</RouterLink>
-                        </li>
-                    </ul>
-                </nav>
+                    <nav>
+                        <ul class="pt-4 pb-6 text-[#141415]">
+                            <li>
+                                <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/">Home</RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/about">About us</RouterLink>
+                            </li>
+                            <li @click="mobileCollectionOpen = true" class="flex justify-between items-center pr-4 border-b border-[#CAD7DF]">
+                                <a class=" px-4 py-3 w-full block" href="#">Collections</a>
+                                <img src="/images/icons/right.svg" alt="Right">
+                            </li>
+                            <li>
+                                <RouterLink class="border-b border-[#CAD7DF] px-4 py-3 w-full block" to="/contact-us">Contact us</RouterLink>
+                            </li>
+                        </ul>
+                    </nav>
 
-                <div class="px-4 text-[#141415]">
-                    <RouterLink to="signin" class="flex items-center space-x-2 py-1">
-                        <img class="w-7 h-7" src="/images/icons/person.svg" alt="Person">
-                        <span>My Account</span>
-                    </RouterLink>
-                    <div class="flex items-center space-x-2 py-1" @click="wishlistOpen = true">
-                        <img class="w-7 h-7" src="/images/icons/wishlist.svg" alt="Wishlist">
-                        <span>My Wishlist</span>
-                    </div>
-                    <div class="flex items-center space-x-2 py-1" @click="cartOpen = true">
-                        <img class="w-7 h-7" src="/images/icons/cart.svg" alt="Cart">
-                        <span>My Cart</span>
+                    <div class="px-4 text-[#141415] pb-8">
+                        <RouterLink to="signin" class="flex items-center space-x-2 py-1">
+                            <img class="w-7 h-7" src="/images/icons/person.svg" alt="Person">
+                            <span>My Account</span>
+                        </RouterLink>
+                        <div class="flex items-center space-x-2 py-1" @click="wishlistOpen = true">
+                            <img class="w-7 h-7" src="/images/icons/wishlist.svg" alt="Wishlist">
+                            <span>My Wishlist</span>
+                        </div>
+                        <div class="flex items-center space-x-2 py-1" @click="cartOpen = true">
+                            <img class="w-7 h-7" src="/images/icons/cart.svg" alt="Cart">
+                            <span>My Cart</span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Transition>
 
             <!-- Collection Menu -->
             <div class="absolute w-full bg-[#FFFFFF] -bottom-80 z-50" :class="{'hidden' : !collectionOpen, 'block': collectionOpen}">
@@ -283,4 +285,33 @@ function closeMobile() {
     border: 10px solid;
     border-color: #0000 #0000 #215373 #0000;
 }
+
+ .v-enter-active {
+    animation: slideIn 1s;
+  }
+
+  @keyframes slideIn {
+    0% {
+      translate: 200px 0;
+    }
+    50% {
+      translate: -15px 0;
+    }
+    100% {
+        translate: 0 0;
+    }
+  }
+  .v-leave-active {
+    animation: slideOut 0.5s;
+  }
+  @keyframes slideOut {
+    from {
+      translate: 0 0;
+      /* Opacity makes the transition fade in.  */
+      /* opacity: 1; */
+    }
+    to {
+      translate: 300px 0;
+    }
+  }
 </style>
